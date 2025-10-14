35c6e094adad8b54bb9db371a76f556f2709db80
Pedro <pedroluizns@hotmail.com>
2025-10-14 10:06:36 -0300

    feat: inicialização do projeto

---
+++ implementação/docker/mysql/init.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE IF NOT EXISTS piece_de_monnaie;
+USE piece_de_monnaie;
---
+++ implementação/src/test/java/com/lab/piece_de_monnaie/PieceDeMonnaieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab.piece_de_monnaie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PieceDeMonnaieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
