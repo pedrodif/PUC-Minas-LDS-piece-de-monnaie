9b9305bfcaef6a9f8b290956e2f0c7a3ddc0a23e
pedrodif <pedrodif@icloud.com>
2025-10-30 18:14:10 -0300

    feat/implementa notifier

--- implementação/frontend/src/js/application/Service.js
+++ implementação/frontend/src/js/application/Service.js
@@ -1,19 +1,25 @@
 import { APIClient } from './APIClient.js'
+import { Notifier } from '../helper/Notifier.js'
 import { RequestHelper } from '../helper/RequestHelper.js'
 
 export class Service {
-    constructor(endpoint, notifier) {
+    constructor({ endpoint, toastMessages }) {
+        this.toastMessages = toastMessages
         this.APIClient = new APIClient(endpoint)
     }
 
     async getAll() {
         const response = await RequestHelper.execute(() => this.APIClient.getAll())
+        Notifier.response(response, 'list', this.toastMessages)
+
         return response
     }
 
     async getById(id) {
         try {
             const response = await RequestHelper.execute(() => this.APIClient.getById(id))
+            Notifier.response(response, 'retrieve', this.toastMessages)
+
             return response
         } catch (error) {
             return { error: error.message }
@@ -23,6 +29,8 @@ export class Service {
     async create(data) {
         try {
             const response = await RequestHelper.execute(() => this.APIClient.post(data))
+            Notifier.response(response, 'create', this.toastMessages)
+
             return response
         } catch (err) {
             return { error: err.message }
@@ -34,6 +42,8 @@ export class Service {
 
         try {
             const response = await RequestHelper.execute(() => this.APIClient.put(id, data))
+            Notifier.response(response, 'update', this.toastMessages)
+
             return response
         } catch (err) {
             return { error: err.message }
@@ -43,6 +53,8 @@ export class Service {
     async delete(id) {
         try {
             const response = await RequestHelper.execute(() => this.APIClient.delete(id))
+            Notifier.response(response, 'delete', this.toastMessages)
+
             return response
         } catch (error) {
             return { error: error.message }
--- implementação/frontend/src/js/presentation/page/login/main.js
+++ implementação/frontend/src/js/presentation/page/login/main.js
@@ -3,16 +3,24 @@ import { Form } from '../../component/Form.js'
 import { Session } from '../../../middleware/Session.js'
 import { Service } from '../../../application/Service.js'
 
-Session.watch()
-const loginService = new Service('/auth')
 const form = new Form(document.querySelector('form'))
+const loginService = new Service({
+    endpoint: '/api/alunos',
+    toastMessages: {
+        create: {
+            error: 'Erro ao realizar login.',
+            success: 'Login realizado com sucesso!',
+        }
+    }
+})
 
-const handleSubmit = (data) =>  {
-    const response = loginService.create(data)
-    
+const handleSubmit = async (data) => {
+    const response = await loginService.create(data)
+    console.log(response)
+
     if (response !== false) {
         Session.create(response)
-        window.location.href = ''
+        // window.location.href = ''
     }
 }
 
