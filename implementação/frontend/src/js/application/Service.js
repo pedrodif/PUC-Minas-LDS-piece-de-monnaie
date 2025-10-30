ca22e4f8e30555565005d387a9bf8b1acdb185d4
pedrodif <pedrodif@icloud.com>
2025-10-30 14:37:50 -0300

    feat/implementa service

--- implementação/frontend/src/js/application/Service.js
+++ implementação/frontend/src/js/application/Service.js
@@ -1,5 +1,52 @@
+import { APIClient } from './APIClient.js'
+import { RequestHelper } from '../helper/RequestHelper.js'
+
+
 export class Service {
-    constructor(parameters) {
-        
+    constructor(endpoint) {
+        this.APIClient = new APIClient(endpoint)
+    }
+
+    async getAll() {
+        const response = await RequestHelper.execute(() => this.APIClient.getAll())
+        return response
+    }
+
+    async getById(id) {
+        try {
+            const response = await RequestHelper.execute(() => this.APIClient.getById(id))
+            return response
+        } catch (error) {
+            return { error: error.message }
+        }
+    }
+
+    async create(data) {
+        try {
+            const response = await RequestHelper.execute(() => this.APIClient.post(data))
+            return response
+        } catch (err) {
+            return { error: err.message }
+        }
+    }
+
+    async update(data) {
+        const { id } = data
+
+        try {
+            const response = await RequestHelper.execute(() => this.APIClient.put(id, data))
+            return response
+        } catch (err) {
+            return { error: err.message }
+        }
+    }
+
+    async delete(id) {
+        try {
+            const response = await RequestHelper.execute(() => this.APIClient.delete(id))
+            return response
+        } catch (error) {
+            return { error: error.message }
+        }
     }
 }
---
+++ implementação/frontend/src/js/helper/RequestHelper.js
@@ -0,0 +1,13 @@
+export class RequestHelper {
+    static async execute(callback) {
+        Loader.getLoader().show()
+
+        try {
+            const response = await callback()
+            await new Promise(resolve => setTimeout(resolve, 1200))
+            return response
+        } finally {
+            Loader.getLoader().hide()
+        }
+    }
+}
