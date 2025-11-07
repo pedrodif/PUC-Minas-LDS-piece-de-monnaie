9469fd1b02bd561dab1050f9ee94a0960ca92444
Pedro <pedroluizns@hotmail.com>
2025-11-07 17:27:47 -0300

    refact: adiciona campo nomeImagem em Vantagem

--- implementação/backend/src/main/java/com/lab/piece_de_monnaie/dto/VantagemResponse.java
+++ implementação/backend/src/main/java/com/lab/piece_de_monnaie/dto/VantagemResponse.java
@@ -5,6 +5,7 @@ import com.lab.piece_de_monnaie.dto.auth.EmpresaParceiraResponse;
 public record VantagemResponse(Long id,
                                Long valor,
                                String descricao,
+                               String nomeImagem,
                                String imagem,
                                EmpresaParceiraResponse empresaParceira) {
 }
