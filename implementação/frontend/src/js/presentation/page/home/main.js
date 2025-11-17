5cf768cd6211ed1a4ebc514c1b1b8a4fcd404c53
pedrodif <pedrodif@icloud.com>
2025-11-17 17:31:14 -0300

    feat/exibe em tela quantidade de moedas de forma dinamica

--- implementação/frontend/src/js/presentation/page/home/main.js
+++ implementação/frontend/src/js/presentation/page/home/main.js
@@ -13,27 +13,40 @@ const figcaption = document.querySelector('figcaption')
 const BUTTON_CONFIGS = {
     ALUNO: {
         text: '<i class="fa-solid fa-hand-holding-heart"></i> Resgatar Vantagens',
-        displayBalance: 'block',
+        balance: {
+            display: 'flex',
+            innerHTML: `<i class="fa-solid fa-coins"></i> ${user.quantidadeMoeda} Moedas`
+        },
         onClick: () => window.location.href = routes.getReward
     },
     EMPRESA: {
         text: '<i class="fa-solid fa-hands-holding-circle"></i> Gerenciar Vantagens',
-        displayBalance: 'none',
+        balance: {
+            display: 'none',
+        },
         onClick: () => window.location.href = routes.manageReward
     },
     PROFESSOR: {
         text: 'Meus Alunos',
-        displayBalance: 'block',
+        displayBalance: {
+            display: 'flex',
+            innerHTML: `<i class="fa-solid fa-coins"></i> ${user.quantidadeMoeda} Moedas`
+        },
         onClick: () => window.location.href = ''
     }
 }
 
-const customizeEnvironment = () => {
+const customizeHome = () => {
     button.innerHTML = BUTTON_CONFIGS[user.tipo].text
     button.onclick = BUTTON_CONFIGS[user.tipo].onClick
-    p.style.display = BUTTON_CONFIGS[user.tipo].displayBalance
+
+    p.style.display = BUTTON_CONFIGS[user.tipo].balance.display
+
+    if (BUTTON_CONFIGS[user.tipo].balance.display === 'flex')
+        p.innerHTML = BUTTON_CONFIGS[user.tipo].balance.innerHTML
+
     figcaption.textContent = `Seja bem vindo(a) ao Pièce de Monnaie ${user.nome}`
 }
 
 header.render()
-customizeEnvironment()
+customizeHome()
